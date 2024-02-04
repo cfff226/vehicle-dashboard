7520d97e57bef058cb38ec3b24361112321a6a86
cfff226 <cazziednb@outlook.com>
2024-02-04 12:14:55 +0000

    Add logic to display a dashboard to the user - Create 4 variables to be displayed on the dashboard and assign them temporary values of 0 - Create a string to be used as a title - Create a nested list including the headers and the first row of variables

--- dash.py
+++ dash.py
@@ -1,5 +1,20 @@
 # Vehicle dashboard program
 
+from tabulate import tabulate
+
+LI = 0
+speed = 0
+car_range = 0
+RI = 0
+
+
+title = "\n\t\t+++++++++ VEHICLE DASHBOARD +++++++++\n"
+table = [["LI", "Speed", "Range", "RI"], [LI, speed, car_range, RI]]
+
+print(f"{title}\n")
+print(tabulate(table, headers="firstrow", tablefmt="pretty"))
+
+
 # Display the menu.
 menu = True
 
@@ -10,8 +25,15 @@ while True:
     1. View engine details
     2. View music selection
     3. View car analytics
-    3. Quit application
+    4. Quit application
 
     Enter selection: """
         )
     )
+    # if user_choice == 1:
+
+    # elif user_choice == 2:
+
+    # elif user_choice == 3:
+
+    # elif user_choice == 4:
