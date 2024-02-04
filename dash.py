2eb84ac38e2b2ff8ae99830d3a4bd33ce5bdd035
cfff226 <cazziednb@outlook.com>
2024-02-04 09:17:04 +0000

    Add logic to display the menu - Add menu variable and set it to True - Add while True statement - Request for the users choice of menu selection and cast this into an integer

--- dash.py
+++ dash.py
@@ -1,1 +1,17 @@
 # Vehicle dashboard program
+
+# Display the menu.
+menu = True
+
+while True:
+    user_choice = int(
+        input(
+            """\nWould you like to:\n
+    1. View engine details
+    2. View music selection
+    3. View car analytics
+    3. Quit application
+
+    Enter selection: """
+        )
+    )
