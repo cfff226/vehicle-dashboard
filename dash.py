d790e14135c8ec74dcaf341463175d992ca104a7
cfff226 <cazziednb@outlook.com>
2024-02-04 12:57:16 +0000

    Add function to display a seatbelt warning - Pass in the argument of seatbelt sensor and assign seatbelt sensor to False - Call the seatbelt warning function when the initial menu displays

--- dash.py
+++ dash.py
@@ -6,19 +6,27 @@ LI = 0
 speed = 0
 car_range = 0
 RI = 0
+# seatbelt_sensor = False
 
 
-title = "\n\t\t+++++++++ VEHICLE DASHBOARD +++++++++\n"
+title = "\n\n\t\t+++++++++ VEHICLE DASHBOARD +++++++++\n"
 table = [["LI", "Speed", "Range", "RI"], [LI, speed, car_range, RI]]
 
-print(f"{title}\n")
-print(tabulate(table, headers="firstrow", tablefmt="pretty"))
+
+# Function to display a warning
+def seatbelt_warning(seatbelt_sensor):
+    if not seatbelt_sensor:
+        print("\n\n\t\tDriver is not wearing a seatbelt\n")
 
 
 # Display the menu.
 menu = True
 
+print(f"{title}\n")
+print(tabulate(table, headers="firstrow", tablefmt="pretty"))
+
 while True:
+    seatbelt_warning(seatbelt_sensor=False)
     user_choice = int(
         input(
             """\nWould you like to:\n
@@ -31,9 +39,13 @@ while True:
         )
     )
     # if user_choice == 1:
+    #     engine_details()
 
     # elif user_choice == 2:
+    # music_selection()
 
     # elif user_choice == 3:
+    # car_analytics()
 
     # elif user_choice == 4:
+    # quit()
